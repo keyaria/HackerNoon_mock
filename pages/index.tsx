de697e89fad35e0bf00b4a32843cd0e507449cb9
Keyaria Walker <keyaria@tablecheck.com>
2021-03-15 20:25:39 +0900

    Added basic nav funtionality and layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,17 @@
 import Link from 'next/link'
+import { Box } from "@chakra-ui/react"
 import Layout from '../components/Layout'
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
+  <Box h="900px" bgColor="red">
     <h1>Hello Next.js 👋</h1>
     <p>
       <Link href="/about">
         <a>About</a>
       </Link>
     </p>
+  </Box>
   </Layout>
 )
 
